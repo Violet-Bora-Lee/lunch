c02a029038bd04020c5fc9ae34cbb125ee814253
Violet Bora Lee <learnerbr@gmail.com>
2020-05-10 01:35:37 +0900

    update, delete people API

--- routes/people.js
+++ routes/people.js
@@ -38,9 +38,57 @@ router.post('/', async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
-  res.send('post people');
 });
 
+// @route     PUT api/people/:id
+// @desc      Update people
+router.put('/:id', async (req, res) => {
+  const { name } = req.body;
+
+  const nameFields = {};
+  if (name) nameFields.name = name;
+
+  try {
+    let person = await Person.findById(req.params.id);
+
+    if(!person) {
+      return res.status(404).json({ msg: '해당하는 사람이 없습니다.'});
+    }
+
+    person = await Person.findByIdAndUpdate(
+      req.params.id,
+      { $set: nameFields },
+      { new: true }
+    );
+
+    res.json(person);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
+
+// @route     DELETE api/people/:id
+// @desc      Delete person
+router.delete('/:id', async (req, res) => {
+
+  try {
+
+    let person = await Person.findById(req.params.id);
+
+    if(!person) return res.status(404).json({ msg: '삭제할 항목을 찾을 수 없습니다.' });
+
+    await Person.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: `${person.name} 이/가 삭제되었습니다.` });
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+});
 
 
 module.exports = router;
